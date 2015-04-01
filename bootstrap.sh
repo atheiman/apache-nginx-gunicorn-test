dcd23b742dfa36dbd7695b0b756a37dba93f1c06
Austin Heiman <atheimanksu@gmail.com>
2015-04-01 13:10:45 -0500

    apachedomain.com:8000 -> nginxdomain.com:80 working

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,13 +7,13 @@ APACHE_VHOST_CONF=$SYNCED_FOLDER/apachedomain.com.conf
 NGINX_CONF_DIR=/etc/nginx
 NGINX_VHOST_CONF=nginx-vhost
 NGINX_DOCS_DIR=$SYNCED_FOLDER/nginxdomain.com
-NGINX_DOC_ROOT=/var/$NGINX_DOCS_DIR
+NGINX_DOC_ROOT=/var/nginxdomain.com
 
 # apachedomain.com and nginxdomain.com
 cp -f $SYNCED_FOLDER/hosts /etc/hosts
 
 apt-get update
-apt-get install -y apache2 nginx
+apt-get install -y apache2 nginx python-pip
 
 
 service apache2 stop
@@ -23,8 +23,9 @@ service nginx stop
 a2dissite 000-default.conf
 a2enmod rewrite
 a2enmod proxy
+a2enmod proxy_http
 cp $APACHE_VHOST_CONF $APACHE_CONF_DIR/sites-available/
-cp ports.conf $APACHE_CONF_DIR/
+cp $SYNCED_FOLDER/ports.conf $APACHE_CONF_DIR/
 a2ensite apachedomain.com.conf
 
 service apache2 start
@@ -36,3 +37,6 @@ ln -s $NGINX_CONF_DIR/sites-available/$NGINX_VHOST_CONF $NGINX_CONF_DIR/sites-en
 cp -r $NGINX_DOCS_DIR $NGINX_DOC_ROOT
 
 service nginx start
+
+
+pip install gunicorn django
